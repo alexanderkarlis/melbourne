71c934956263e1a99c27766b84e6d00b0717fe97
Karlis Alexander <KAR10942@dslocal.com>
2018-04-12 17:21:49 -0400

    decision tree sklearn

--- decision_tree.py
+++ decision_tree.py
@@ -1,11 +1,36 @@
 import pandas as pd
+from sklearn.tree import DecisionTreeRegressor
 
-mb_file_path = '../input/train.csv'
+mb_file_path = 'train.csv'
 mb_data = pd.read_csv(mb_file_path)
-print(mb_data.columns)
+mb_data = mb_data.dropna()
+for index in mb_data.columns:
+	print(index)
 
 price_data = mb_data.SalePrice
 print(price_data.head())
 
-cols = ['Lotsize','BuildingArea']
+cols = ['Rooms','Price']
 new_cols = mb_data[cols]
+print(new_cols)
+#print(new_cols.describe())
+
+# choosing a Prediction Target
+y = mb_data.Price
+
+# choosing predictors 
+melbourne_predictors = ['Rooms','Bathroom', 'Landsize', 'BuildingArea', 'YearBuilt',
+'Lattitude', 'Longtitude']
+X = mb_data[melbourne_predictors]
+
+# define model
+melbourne_model = DecisionTreeRegressor()
+
+# fit model
+melbourne_model.fit(X, y)
+
+# making predictions
+print("Making predictions for the following 5 houses...\n")
+print(X.head())
+print("The predictions are\n")
+print(melbourne_model.predict(X.head()))
